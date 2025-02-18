96d1ff5710e396639fe51c3c33983d9cd5d3e278
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-18 17:17:39 -0300

    feat: creating expense information

--- scripts.js
+++ scripts.js
@@ -54,15 +54,30 @@ function expenseAdd(newExpense) {
   try {
     // Cria o elemento para adicionar o item (li) na lista (ul).
     const expenseItem = document.createElement("li")
-    expenseItem.classList("expense")
+    expenseItem.classList.add("expense")
 
     // Cria o ícone da categoria.
     const expenseIcon = document.createElement("img")
     expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
     expenseIcon.setAttribute("alt",  newExpense.category_name)
 
+    // Cria a info da despesa.
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // Cria o nome da despesa.
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // Cria categoria da despesa
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Adiciona nome e categoria na div das informações da despesa.
+    expenseInfo.append(expenseName, expenseCategory)
+    
     // Adiciona as informações no item.
-    expenseItem.append(expenseIcon)
+    expenseItem.append(expenseIcon, expenseInfo)
 
     // Adiciona o item na lista.
     expenseList.append(expenseItem)
