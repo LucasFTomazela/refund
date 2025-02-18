bce490f849d3c294fd9c581c547bbb4daee883f9
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-18 20:45:28 -0300

    feat: creating the remove icon

--- scripts.js
+++ scripts.js
@@ -83,8 +83,14 @@ function expenseAdd(newExpense) {
       <small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}
     `
     
+    // Cria o ícone de remover
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "Remover")
+
     // Adiciona as informações no item.
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     // Adiciona o item na lista.
     expenseList.append(expenseItem)
