6ce11dd40d6c1c727ef124d31318ddb4029508ee
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-19 16:12:31 -0300

    feat: clearing form fields

--- scripts.js
+++ scripts.js
@@ -99,6 +99,9 @@ function expenseAdd(newExpense) {
     // Adiciona o item na lista.
     expenseList.append(expenseItem)
 
+    // Limpa o formulário após adicionar um novo item
+    formClear()
+
     // Atualiza os totais.
     updateTotals()
 
@@ -171,3 +174,13 @@ expenseList.addEventListener("click", function(event) {
   // Atualiza os totais
   updateTotals()
 })
+
+function formClear(){
+  // Limpa os inputs
+  expense.value = ""
+  category.value = ""
+  amount.value = ""
+
+  // Coloca o foco no input de expense.
+  expense.focus()
+}
